91e20fb40e998fa4c0dd321fe70519b47ae22f98
JaelinMckethan <158495532+JaelinMckethan@users.noreply.github.com>
2024-02-27 21:19:21 -0600

    Added Main Menu Settings and Timer

--- scenes/levels/level 1/Level1.gd
+++ scenes/levels/level 1/Level1.gd
@@ -2,18 +2,46 @@ extends Node2D
 
 @onready var pause_menu = $PauseMenu
 var paused = false
+var timer_running = true
+var start_time = 0
+var elapsed_time = 0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("pause"):
 		pauseMenu()
+	if timer_running:
+		elapsed_time += delta
 
 func pauseMenu():
 	if paused:
 		pause_menu.hide()
 		Engine.time_scale = 1
+		timer_running = true
+		print("Timer started.")
 	else:
 		pause_menu.show()
 		Engine.time_scale = 0
+		timer_running = false
+		print("Timer stopped.")
+		print("Elapsed time:", elapsed_time, "seconds.")
 	
 	paused = !paused
+
+func _start_timer():
+	start_time = 0
+	elapsed_time = 0
+	timer_running = true
+	$Timer.start()  # Start the timer when the scene is accessed
+
+
+
+func _input(event):
+	if Input.is_action_just_pressed("timer"):
+		if timer_running:
+			timer_running = false
+			print("Timer stopped.")
+			print("Elapsed time:", elapsed_time, "seconds.")
+		else:
+			timer_running = true
+			print("Timer started.")
--- scripts/menus/options_menu.gd
+++ scripts/menus/options_menu.gd
@@ -1,11 +1,72 @@
 extends Control
 
+# Variables
+@onready var options = $Options
+@onready var video = $Video
+@onready var audio = $Audio
+@onready var music = $AudioManager/Music
+@onready var sfx =  $AudioManager/SFX
 
+var music_bus_name = "Music"
+var music_bus_index = 0
+var sfx_bus_name = "sfx"
+var sfx_bus_index = 0
 
+@onready var musicPlayer: AudioStreamPlayer = $AudioManager/Music
+@onready var sfxPlayer: AudioStreamPlayer = $AudioManager/SFX
 
+# Functions
 func _on_volume_pressed():
 	pass # Replace with function body.
 
+func _on_back_to_main_pressed():
+	get_tree().change_scene_to_file("res://scenes/menus/main_menu.tscn") # Replace with function body.
 
-func _on_back_pressed():
-	get_tree().change_scene_to_file("res://scenes/menus/main_menu.tscn")
+func show_and_hide(first, second):
+	first.show()
+	second.hide()
+
+func _on_audio_pressed():
+	show_and_hide(audio, options)
+
+func _on_video_pressed():
+	show_and_hide(video, options)
+
+func _on_back_to_option_video_pressed():
+	show_and_hide(options, video)
+
+func _on_back_to_option_audio_pressed():
+	show_and_hide(options, audio)
+
+func _on_fullscreen_toggled(toggled_on):
+	if toggled_on:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+
+func _get_Music_Name():
+	music_bus_index = AudioServer.get_bus_index(music_bus_name)
+	print("Custom audio bus index:", music_bus_index)
+
+func _on_music_value_changed(value):
+	# Ensure the custom bus index is valid
+		# Adjust the volume of the custom bus based on the slider's value
+	AudioServer.set_bus_volume_db(music_bus_index, value)
+	print("Music volume changed to:", value)
+
+func _get_SFX_Name():
+	sfx_bus_index = AudioServer.get_bus_index(sfx_bus_name)
+	print("SFX bus index:", sfx_bus_index)
+
+func _on_sound_fx_value_changed(value):
+	# Ensure the custom bus index is valid
+	# Adjust the volume of the custom bus based on the slider's value
+	AudioServer.set_bus_volume_db(sfx_bus_index, value)
+	print("SFX volume changed to:", value)
+
+# Button event handlers
+func _on_test_music_button_pressed():
+	musicPlayer.play()
+
+func _on_test_sfx_button_pressed():
+	sfxPlayer.play()
