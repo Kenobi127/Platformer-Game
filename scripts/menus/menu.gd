87f4ed7646ab4f093f62c8761647635fc4f3af29
Kenobi127 <mateolm1207@gmail.com>
2024-03-23 15:14:49 -0500

    fixed tilemaps and everything left working

--- scripts/menus/menu.gd
+++ scripts/menus/menu.gd
@@ -18,12 +18,12 @@ func _on_quit_game_pressed():
 	get_tree().quit()
 
 func _on_credits_view_pressed():
-	# Fade out before closing
+	#Fade out before closing
 	#SceneManager.button_chime()
 	#SceneManager.fade_out()
 	#await SceneManager.faded_out
-	# Make credits appear and menu disappear
-	$TitleNameMap.visible = false
+	#Make credits appear and menu disappear
+	$MainMenuTileMap.visible = false
 	$CreditsBack.visible = true
 	$CreditsBack/CreditsCanvas.visible = true
 	#SceneManager.fade_in()
@@ -35,7 +35,7 @@ func _on_credits_back_pressed():
 	#SceneManager.fade_out()
 	#await SceneManager.faded_out
 	# Make credits disappear and menu appear
-	$TitleNameMap.visible = true
+	$MainMenuTileMap.visible = true
 	$CreditsBack.visible = false
 	$CreditsBack/CreditsCanvas.visible = false
 	#SceneManager.fade_in()
