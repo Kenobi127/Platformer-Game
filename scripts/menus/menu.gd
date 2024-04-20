a618f0427e4d6422cdc9b77685310e37816eaba3
Kenobi127 <mateolm1207@gmail.com>
2024-04-20 01:30:01 -0500

    menu music stop on arena

--- scripts/menus/menu.gd
+++ scripts/menus/menu.gd
@@ -23,4 +23,5 @@ func _on_quit_game_pressed():
 
 func _on_arena_pressed() -> void:
 	SceneManager.button_sound.play()
+	SceneManager.menu_music.stop()
 	SceneManager.load_scene("res://scenes/levels/testing mechanics/testing_mechanics.tscn")
